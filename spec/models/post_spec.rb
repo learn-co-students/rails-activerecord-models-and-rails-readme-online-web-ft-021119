fe0b55b7c94c59e7625070b8974145996d423f3c
github username <github email address>
2019-04-17 15:39:19 -0400

    Done.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -9,7 +9,7 @@ describe Post do
 
   it 'has a summary' do
     post = Post.create!(title: "My title", description: "The post description")
-    expect post(post.post_summary).to eq("My title - The post description")
+    expect(post.post_summary).to eq("My title - The post description")
   end
 
 end
